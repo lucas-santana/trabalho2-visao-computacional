87bb7df95616b5237a181bd3b5c0eec148f9eb2e
Lucas Santana <lucasg4x@gmail.com>
2023-08-29 15:53:01 -0300

    save fig

--- plot.py
+++ plot.py
@@ -2,6 +2,8 @@
 import matplotlib.pyplot as plt
 
 def plot_acc(train_accu, eval_accu):
+    f = plt.figure() # gera uma figura do gr ́afico (antes de desenh ́a-lo)
+
     plt.plot(train_accu,'-o')
     plt.plot(eval_accu,'-o')
     plt.xlabel('epoch')
@@ -10,8 +12,11 @@ def plot_acc(train_accu, eval_accu):
     plt.title('Train vs Valid Accuracy')
 
     plt.show()
+    f.savefig('results/acc.pdf')
     
 def plot_loss(train_losses, eval_losses):
+    f = plt.figure() # gera uma figura do gr ́afico (antes de desenh ́a-lo)
+
     plt.plot(train_losses,'-o')
     plt.plot(eval_losses,'-o')
     plt.xlabel('epoch')
@@ -20,6 +25,7 @@ def plot_loss(train_losses, eval_losses):
     plt.title('Train vs Valid Losses')
 
     plt.show()
+    f.savefig('results/loss.pdf')
     
 # def plot_confusion_matrix():
     
