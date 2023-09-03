e74fad2c80913593f892b1a0631a2ccc166aba69
Lucas Santana <lucasg4x@gmail.com>
2023-09-03 02:45:39 -0300

    VGG11

--- networks/VGG11.py
+++ networks/VGG11.py
@@ -1,8 +1,8 @@
 from torch import nn
 
-class VGG16(nn.Module):
+class VGG11(nn.Module):
     def __init__(self, num_classes, gray_scale=False):
-        super(VGG16, self).__init__()
+        super(VGG11, self).__init__()
         
         self.gray_scale = gray_scale
         self.num_classes = num_classes
@@ -11,90 +11,53 @@ class VGG16(nn.Module):
             in_channels = 1
         else:
             in_channels = 3
-        
+
         # conv3-64
         self.layer1 = nn.Sequential(
             nn.Conv2d(1*in_channels, 64*in_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64*in_channels),
-            nn.ReLU())
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
         
-        # conv3-64
+        # conv3-128
         self.layer2 = nn.Sequential(
-            nn.Conv2d(64*in_channels, 64*in_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64*in_channels),
-            nn.ReLU(), 
-            nn.MaxPool2d(kernel_size = 2, stride = 2))
-        
-        # conv3-128
-        self.layer3 = nn.Sequential(
             nn.Conv2d(64*in_channels, 128*in_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128*in_channels),
-            nn.ReLU())
-        
-        # conv3-128
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(128*in_channels, 128*in_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128*in_channels),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 2, stride = 2))
         
-        
+
         # conv3-256 
-        self.layer5 = nn.Sequential(
+        self.layer3 = nn.Sequential(
             nn.Conv2d(128*in_channels, 256*in_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256*in_channels),
             nn.ReLU())
         
         # conv3-256 
-        self.layer6 = nn.Sequential(
+        self.layer4 = nn.Sequential(
             nn.Conv2d(256*in_channels, 256*in_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256*in_channels),
-            nn.ReLU())
-        # conv3-256 
-        self.layer7 = nn.Sequential(
-            nn.Conv2d(256*in_channels, 256*in_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256*in_channels),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 2, stride = 2))
         
+        # conv3-512
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(256*in_channels, 512*in_channels, kernel_size=3, stride=1, padding=1),
+            nn.ReLU())
+        
+        # conv3-512
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(512*in_channels, 512*in_channels, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        
+        # conv3-512
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(512*in_channels, 512*in_channels, kernel_size=3, stride=1, padding=1),
+            nn.ReLU())
         
         # conv3-512
         self.layer8 = nn.Sequential(
-            nn.Conv2d(256*in_channels, 512*in_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512*in_channels),
-            nn.ReLU())
-        
-        # conv3-512
-        self.layer9 = nn.Sequential(
             nn.Conv2d(512*in_channels, 512*in_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512*in_channels),
-            nn.ReLU())
-        
-        # conv3-512
-        self.layer10 = nn.Sequential(
-            nn.Conv2d(512*in_channels, 512*in_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512*in_channels),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 2, stride = 2))
         
-        # conv3-512
-        self.layer11 = nn.Sequential(
-            nn.Conv2d(512*in_channels, 512*in_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512*in_channels),
-            nn.ReLU())
-        
-        # conv3-512
-        self.layer12 = nn.Sequential(
-            nn.Conv2d(512*in_channels, 512*in_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512*in_channels),
-            nn.ReLU())
-        
-        # conv3-512
-        self.layer13 = nn.Sequential(
-            nn.Conv2d(512*in_channels, 512*in_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512*in_channels),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 2, stride = 2))
         
         # FC-4096
         self.fc = nn.Sequential(
@@ -121,11 +84,6 @@ class VGG16(nn.Module):
         out = self.layer6(out)
         out = self.layer7(out)
         out = self.layer8(out)
-        out = self.layer9(out)
-        out = self.layer10(out)
-        out = self.layer11(out)
-        out = self.layer12(out)
-        out = self.layer13(out)
         out = out.reshape(out.size(0), -1)
         out = self.fc(out)
         out = self.fc1(out)
