1654930b5f4f4512ff0251bfffbbc2a76b0bdcb0
Lucas Santana <lucasg4x@gmail.com>
2023-09-04 02:13:07 -0300

    update

--- automatic_training_69.py
+++ automatic_training_69.py
@@ -4,7 +4,7 @@ import subprocess
 from util import check_exp_exist
 
 
-experiments_id = [61,62,63,64,69,71]
+experiments_id = [61,62,63,64,71]
 
 script_name = open("main.py")
 script = script_name.read()
